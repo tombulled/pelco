1f6b72c12c7f3f7f8ac79de9d74726111f81e247
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2023-02-08 22:09:18 +0000

    Add UInt8 and UInt16 type annotations

---
+++ pelco/d/typing.py
@@ -0,0 +1,4 @@
+from typing_extensions import TypeAlias
+
+UInt8: TypeAlias = int
+UInt16: TypeAlias = int
--- pelco/d/utils.py
+++ pelco/d/utils.py
@@ -1,25 +1,26 @@
 from typing import Sequence, Tuple
 
 from .constants import UINT8_MAX, UINT8_SIZE
+from .typing import UInt8, UInt16
 from .validators import validate_uint8, validate_uint16
 
 
-def calculate_checksum(bytes: Sequence[int], /) -> int:
-    checksum: int = 0x00
+def calculate_checksum(values: Sequence[UInt8], /) -> UInt8:
+    checksum: UInt8 = 0x00
 
-    byte: int
-    for byte in bytes:
-        validate_uint8(byte)
+    value: UInt8
+    for value in values:
+        validate_uint8(value)
 
-        checksum += byte
+        checksum += value
 
     return checksum % (UINT8_MAX + 1)
 
 
-def sep_uint16(data: int, /) -> Tuple[int, int]:
+def sep_uint16(data: UInt16, /) -> Tuple[UInt8, UInt8]:
     validate_uint16(data)
 
-    data_1: int = (data >> UINT8_SIZE) & UINT8_MAX
-    data_2: int = data & UINT8_MAX
+    data_1: UInt8 = (data >> UINT8_SIZE) & UINT8_MAX
+    data_2: UInt8 = data & UINT8_MAX
 
     return (data_1, data_2)
