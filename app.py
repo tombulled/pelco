3b094c668803ad9875e386506379e04e6276abe4
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2023-02-08 22:15:40 +0000

    Tidy up

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 import serial.tools.list_ports
 
-import pelco
-import pelcod
-import pelco.p
 from pelcod.main import PelcoD
 
 
@@ -24,25 +21,25 @@ p = PelcoD(
 SPEED: int = 0x00
 
 commands = {
-    "fn": lambda: p.focus_near(),  # focus near
-    "ff": lambda: p.focus_far(),  # focus far
-    "zt": lambda: p.zoom_tele(),  # zoom tele
-    "zw": lambda: p.zoom_wide(),  # zoom wide
-    "u": lambda: p.tilt_up(SPEED),  # tilt up
-    "d": lambda: p.tilt_down(SPEED),  # tilt down
-    "l": lambda: p.pan_left(SPEED),  # pan left
-    "r": lambda: p.pan_right(SPEED),  # pan right
-    "": lambda: p.stop(),  # stop
-    "ws": lambda: p.set_auxiliary_relay(1),  # start wiper
-    "we": lambda: p.clear_auxiliary_relay(1),  # stop wiper
-    "180": lambda: p.flip_180_about(),  # flip 180 degrees
-    "m": lambda: p.set_auxiliary_relay(95),  # flip 180 degrees
-    "sp1": lambda: p.set_preset(1),  # set preset 1
-    "sp2": lambda: p.set_preset(2),  # set preset 1
-    "sp3": lambda: p.set_preset(3),  # set preset 1
-    "gp1": lambda: p.go_to_preset(1),  # go to preset 1
-    "gp2": lambda: p.go_to_preset(2),  # go to preset 1
-    "gp3": lambda: p.go_to_preset(3),  # go to preset 1
+    "fn": lambda: p.focus_near(),             # focus near
+    "ff": lambda: p.focus_far(),              # focus far
+    "zt": lambda: p.zoom_tele(),              # zoom tele
+    "zw": lambda: p.zoom_wide(),              # zoom wide
+    "u": lambda: p.tilt_up(SPEED),            # tilt up
+    "d": lambda: p.tilt_down(SPEED),          # tilt down
+    "l": lambda: p.pan_left(SPEED),           # pan left
+    "r": lambda: p.pan_right(SPEED),          # pan right
+    "": lambda: p.stop(),                     # stop
+    "ws": lambda: p.set_auxiliary_relay(1),   # start wiper
+    "we": lambda: p.clear_auxiliary_relay(1), # stop wiper
+    "180": lambda: p.flip_180_about(),        # flip 180 degrees
+    "m": lambda: p.set_auxiliary_relay(95),   # open menu
+    "sp1": lambda: p.set_preset(1),           # set preset 1
+    "sp2": lambda: p.set_preset(2),           # set preset 2
+    "sp3": lambda: p.set_preset(3),           # set preset 3
+    "gp1": lambda: p.go_to_preset(1),         # go to preset 1
+    "gp2": lambda: p.go_to_preset(2),         # go to preset 2
+    "gp3": lambda: p.go_to_preset(3),         # go to preset 3
 }
 
 while True:
